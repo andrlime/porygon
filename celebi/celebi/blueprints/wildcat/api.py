59b11e44b2ed4b594a9917a4afacbc332a190ad1
Andrew Li <andrewli@u.northwestern.edu>
2025-01-19 16:40:19 -0600

    fix production issues with email-list API for /wildcat

--- celebi/celebi/blueprints/wildcat/api.py
+++ celebi/celebi/blueprints/wildcat/api.py
@@ -11,13 +11,14 @@ from flask_cors import cross_origin
 from celebi.core.flask.config import AppConfig
 from celebi.core.util import make_json_response
 from celebi.core.util import get_body_field
+from celebi.core.exceptions import RequestValueError
 
 
 bp = Blueprint("wildcat", __name__)
 
 
-@bp.route("/subscribe", methods=["POST"])
-@cross_origin()
+@bp.route("/subscribe", methods=["POST", "OPTIONS"])
+@cross_origin(origins="*")
 def subscribe_new_email():
     """
     Subscribes a new user to the Wildhacks email list
@@ -27,9 +28,12 @@ def subscribe_new_email():
     if not request_body:
         return make_json_response("Request did not contain a JSON body", HTTPStatus.BAD_REQUEST)
 
-    first_name = get_body_field(request_body, "first_name")
-    last_name = get_body_field(request_body, "last_name")
-    email_address = get_body_field(request_body, "email_address")
+    try:
+        first_name = get_body_field(request_body, "first_name")
+        last_name = get_body_field(request_body, "last_name")
+        email_address = get_body_field(request_body, "email_address")
+    except RequestValueError as e:
+        return make_json_response(e, HTTPStatus.BAD_REQUEST)
 
     app_config = AppConfig()
     api_key = app_config.get_environment_variable("MAILCHIMP_API_KEY")
@@ -47,9 +51,12 @@ def subscribe_new_email():
 
     request_timeout_seconds = 5
     res = requests.post(mailchimp_url, auth=("hi", api_key), json=post_request_body, timeout=request_timeout_seconds)
+    response_title = res.json().get("title", "NO TITLE")
 
     if res.status_code == 200:
         return make_json_response(f"Successfully subscribed user {email_address}", HTTPStatus.OK)
+    elif response_title == "Member Exists":
+        return make_json_response(f"User {email_address} already exists", HTTPStatus.CONFLICT)
     elif res.status_code == 400:
         return make_json_response(f"Failed to subscribe user {email_address}", HTTPStatus.INTERNAL_SERVER_ERROR)
     else:
--- celebi/celebi/core/flask/factory.py
+++ celebi/celebi/core/flask/factory.py
@@ -3,6 +3,7 @@ App factory to create a Flask app object
 """
 
 from flask import Flask
+from flask_cors import CORS
 
 from celebi.blueprints import all_blueprints
 
@@ -13,6 +14,7 @@ def create_flask_app():
     """
 
     app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
     for bp in all_blueprints():
         app.register_blueprint(bp.blueprint, url_prefix=f"/{bp.path}")
