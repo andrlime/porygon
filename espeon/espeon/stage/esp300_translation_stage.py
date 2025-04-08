1b34077a017f6fbd1695723c25528c6cc0cb3247
Andrew Li <andrewli@u.northwestern.edu>
2025-04-08 15:45:13 -0500

    minor refactor of ESP300 class

--- espeon/espeon/stage/esp300_translation_stage.py
+++ espeon/espeon/stage/esp300_translation_stage.py
@@ -10,6 +10,7 @@ import logging
 import serial
 
 import espeon.common as constants
+import espeon.util as utilities
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
@@ -25,38 +26,10 @@ class ESP300TranslationStage:
         self.axis = axis
 
         self.write(b"VER\r")
-        self.fetch_name()
+        self.get_name()
 
         logger.info(f"Connected to device {self.device_name} ({self.serial_number})")        
         atexit.register(self.close)
-    
-    @staticmethod
-    def encode_command(cmd_string: str):
-        """
-        Encodes a string command into bytes
-        """
-        logger.debug(f"Encoding command {cmd_string}")
-        return f"{cmd_string}\r".encode()
-
-    def fetch_name(self):
-        """
-        Sends (axis)ID?, e.g. 1ID?, to get the device name and serial number
-        """
-        cmd = ESP300TranslationStage.encode_command(
-            cmd_string=f"{self.axis}ID?"
-        )
-        response = self.write(cmd)
-        response_split = response.split(", ")
-        device_name, serial_number = response_split[0], response_split[1]
-        logger.debug(f"Fetched device name {device_name}, serial number {serial_number}")
-        self.device_name = device_name
-        self.serial_number = serial_number
-    
-    def get_name(self):
-        """
-        Returns device name and serial number as a tuple
-        """
-        return self.device_name, self.serial_number
 
     def write(self, cmd: bytes):
         """
@@ -74,3 +47,17 @@ class ESP300TranslationStage:
         """
         logger.info(f"Closing connection to {self.device_name}")
         self.device.close()
+    
+    def get_name(self):
+        """
+        Returns device name and serial number as a tuple
+        """
+        cmd = utilities.encode_command(f"{self.axis}ID?")
+        response = self.write(cmd)
+        response_split = response.split(", ")
+        device_name, serial_number = response_split[0], response_split[1]
+        logger.debug(f"Fetched device name {device_name}, serial number {serial_number}")
+        self.device_name = device_name
+        self.serial_number = serial_number
+        return device_name, serial_number
+    
