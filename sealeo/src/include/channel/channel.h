747e6f5d5fc05c7a416a891f5a1c6e5002d01ce5
Andrew Li <andrewli@u.northwestern.edu>
2025-04-25 21:04:57 -0500

    silly changes with semaphore

--- sealeo/src/include/channel/channel.h
+++ sealeo/src/include/channel/channel.h
@@ -1,52 +1,54 @@
 #include <types.h>
 
 #include <condition_variable>
+
 #include <iostream>
 #include <mutex>
+#include <semaphore>
 
 namespace sealeo {
 namespace channel {
 
 template <typename T>
 class Channel {
-private:
-    std::condition_variable sender_cv, receiver_cv;
-    bool sender_waiting = false;
-    bool receiver_waiting = false;
+    using value_t = T;
 
-    T value;
-    std::mutex mtx; // used for shared access to value
+private:
+    value_t value;
+    std::binary_semaphore sem_sender_ready{0}, sem_receiver_ready{0};
+
+    inline void
+    set_value(const value_t& v)
+    {
+        value = v;
+    }
+
+    inline value_t
+    get_value() const
+    {
+        return value;
+    }
 
 public:
     // Reads a value from the channel. If there is no value, blocks until there is one.
-    T recv() {
-        std::unique_lock<std::mutex> lock(mtx);
-
-        while (!sender_waiting) {
-            receiver_waiting = true;
-            receiver_cv.wait(lock);
-        }
-
-        T result = value;
-        receiver_waiting = false;
-        sender_cv.notify_one();
+    value_t
+    recv()
+    {
+        sem_sender_ready.release();
+        auto result = get_value();
+        sem_receiver_ready.acquire();
         return result;
     }
 
     // Writes a value to the channel. Blocks until success.
-    void send(const T& val) {
-        std::unique_lock<std::mutex> lock(mtx);
-
-        while (!receiver_waiting) {
-            sender_waiting = true;
-            sender_cv.wait(lock);
-        }
-
-        value = val;
-        sender_waiting = false;
-        receiver_cv.notify_one();
+    void
+    send(const value_t& val)
+    {
+        sem_sender_ready.acquire();
+        set_value(val);
+        sem_receiver_ready.release();
     }
 };
 
-}
+} // namespace channel
 } // namespace sealeo
--- sealeo/src/main.cc
+++ sealeo/src/main.cc
@@ -6,8 +6,11 @@
 using sealeo::channel::Channel;
 
 template <typename T>
-void sender(std::vector<T> numbers, std::shared_ptr<Channel<T>> channel) {
+void
+sender(std::vector<T> numbers, std::shared_ptr<Channel<T>> channel)
+{
     for (T elem : numbers) {
+        std::cout << "Sent " << elem << std::endl;
         channel->send(elem);
     }
 
@@ -15,24 +18,35 @@ void sender(std::vector<T> numbers, std::shared_ptr<Channel<T>> channel) {
 }
 
 template <typename T>
-void receiver(std::shared_ptr<Channel<T>> channel, size_t count) {
-    for (size_t _ = 0; _ < count; _ ++) {
+void
+receiver(std::shared_ptr<Channel<T>> channel, size_t count)
+{
+    while (true) {
         T val = channel->recv();
-        std::cout << val << std::endl;
+        std::cout << "Received " << val << std::endl;
     }
 
     return;
 }
 
-int main() {
+int
+main()
+{
+#ifdef _LIBCPP_STD_VER
+    std::cout << "_LIBCPP_STD_VER = " << _LIBCPP_STD_VER << "\n";
+#else
+    std::cout << "_LIBCPP_STD_VER is NOT defined\n";
+#endif
+
     auto c = std::make_shared<Channel<int>>();
     std::vector<int> values{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     std::thread thread1(sender<int>, values, c);
     std::thread thread2(receiver<int>, c, values.size());
 
-    thread1.join();
-    thread2.join();
+    // thread1.join();
+    // thread2.join();
+    while (true) {}
 
     return 0;
 }
